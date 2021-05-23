8ec2cde1932e0b5c6eba6a8e91caa19108ce1070
Tomer <tomersha717@hotmail.com>
2021-05-23 14:52:18 +0300

    MyChanges

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms'
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,13 +13,25 @@ import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +39,10 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [AccountService],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -15,7 +17,10 @@ export class NavComponent implements OnInit {
   
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService, 
+    private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -31,14 +36,18 @@ export class NavComponent implements OnInit {
   login(){
     //console.log(this.model);
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);      
+      console.log(response);
+      this.router.navigateByUrl('/members');      
     },error => {
       console.log(error);
+      this.toastr.error(error.error);
+      
     });
   }
 
   logout(){
-    this.accountService.logout();    
+    this.accountService.logout(); 
+    this.router.navigateByUrl('/');
   }
 
 
