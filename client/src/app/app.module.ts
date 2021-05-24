00822532a8d85e67d2961b3f6362c48bcfe6654b
Tomer <tomersha717@hotmail.com>
2021-05-24 11:55:25 +0300

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 
 
@@ -39,10 +40,7 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    SharedModule
   ],
   providers: [AccountService],
   bootstrap: [AppComponent]
