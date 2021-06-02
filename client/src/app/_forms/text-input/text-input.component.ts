9c9809bcf4cf8579aa13354a325a894772064edf
Tomer <tomersha717@gmail.com>
2021-06-03 01:11:20 +0300

    ReactiveForm_03062021_01:10

--- client/src/app/_forms/text-input/text-input.component.ts
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.css']
 })
-export class TextInputComponent implements OnInit {
+export class TextInputComponent implements ControlValueAccessor {
 
-  constructor() { }
+  @Input() lable: string;
+  @Input() type = 'text';
 
-  ngOnInit(): void {
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
   }
+
+
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
+  }
+
+
+
 
 }
