4ce166c4d09da908c0da06f583b03e10bf857d9d
Tomer <tomersha717@gmail.com>
2021-06-02 09:44:03 +0300

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -52,4 +52,10 @@ export class MembersService {
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
 
+
+
+  deleltePhoto(photoId: number){
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
 }
