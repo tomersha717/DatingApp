718e8ac0d051a7d963c89c7c826b6e076c5dae8b
Tomer <tomersha717@gmail.com>
2021-06-10 19:50:37 +0300

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,6 +23,9 @@ namespace API.Entities
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<UserLike> MessagesReceived { get; set; }
+        
 
     }
 }
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -13,7 +13,7 @@ export class ListsComponent implements OnInit {
   members: Partial<Member[]>;
   predicate = 'liked';
   pageNumber = 1;
-  pageSize = 5;
+  pageSize = 6;
   pagination: Pagination;
 
   constructor(private memberService: MembersService) { }
