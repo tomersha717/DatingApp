196113b81dfe8b300d6e6cbb6cef304ad77e35d0
Tomer <tomersha717@gmail.com>
2021-06-18 15:19:07 +0300

    SignalR_UpdatePoint_18062021_15:20

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
+using API.SignalR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -47,7 +48,7 @@ namespace API
 
             services.AddControllers();
             services.AddIdentityServices(_configuration);
-
+            services.AddSignalR();
 
 
             services.AddSwaggerGen(c =>
@@ -74,7 +75,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             
             app.UseAuthentication();
             app.UseAuthorization();
@@ -82,6 +83,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PresenceHub>("hubs/presence");
             });
         }
     }
